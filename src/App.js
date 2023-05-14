670979a8803ec08bc83fd8097d02b7e08efd5c52
Aditya Danturthi <aditya2096@gmail.com>
2023-05-14 20:44:03 -0230

    font size

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function Footer(){
 function List(){
   return(
     <div className="flex flex-col items-center">
-      <h1 className='font-extrabold text-5xl p-2'>Reasons I'm excited to learn React</h1>
+      <h1 className='font-extrabold text-4xl p-2'>Reasons I'm excited to learn React</h1>
      <ol className="list-decimal list-outside text-2xl font-medium px-6 py-2 ms-8">
       <li>High demand in the job market</li>
       <li>Reusable and modular components</li>
